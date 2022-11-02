14dcbdef73e1d5119b9ccff0ea0a54b7e7f9dc77
Sergei Trofimov <divisor.null@yandex.com>
2022-11-03 00:00:28 +0300

    1. Тесты на класс Bun 2. Тесты на класс Ingredient 3. Тесты на Burger

--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -3,33 +3,66 @@ package praktikum;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BurgerTest {
-    Burger burger = new Burger();
-    @Mock
-    private Bun bun;
-    private Ingredient ingredient;
+
+     Bun bun= Mockito.mock(Bun.class);
+
+     Ingredient ingredient = Mockito.mock(Ingredient.class);
+
+
 
     @Test //addIngredient
     public void addIngredientTest() {
+        Burger burger = new Burger();
         burger.addIngredient(ingredient);
         assertEquals("Размер массива не совпадает", burger.ingredients.size(), 1);
     }
 
     @Test // removeIngredient
     public void removeIngredientTest() {
+        Burger burger = new Burger();
         for (int i = 0; i < 5; i++) {
             burger.addIngredient(ingredient);
         }
-        burger.ingredients.remove(0);
+        burger.removeIngredient(0);
         assertEquals("Размер массива не совпадает", burger.ingredients.size(), 4);
     }
 
-    //moveIngredient
-    //getPrice()
+    @Test //moveIngredient
+    public void moveIngredientTest() {
+        Burger burger = new Burger();
+        for (int i = 0; i < 5; i++) {
+            burger.addIngredient(ingredient);
+        }
+        burger.moveIngredient(0, 4);
+       // Mockito.verify(burger.ingredients, Mockito.times(1)).add(ingredient);
+    }
+
+    @Test //getPrice()
+    public void getPriceTest(){
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+       List<Ingredient> ingredients = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            burger.addIngredient(ingredient);
+        }
+        Mockito.when(bun.getPrice()).thenReturn(5f);
+     Mockito.when(ingredient.getPrice()).thenReturn(1f);
+       System.out.println(burger.getPrice());
+    }
+
     // getReceipt()
+    public void getReceiptTest()
+    {
+
+    }
 }
