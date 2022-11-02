161066c4d904213878b6583726b887b32f718237
Sergei Trofimov <divisor.null@yandex.com>
2022-11-03 00:22:24 +0300

    1. Тесты на класс Bun 2. Тесты на класс Ingredient 3. Тесты на Burger

--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -60,9 +60,19 @@ public class BurgerTest {
        System.out.println(burger.getPrice());
     }
 
-    // getReceipt()
-    public void getReceiptTest()
+    @Test
+    public void getReceiptTest()// getReceipt()
     {
-
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        for (int i = 0; i < 5; i++) {
+            burger.addIngredient(ingredient);
+        }
+        Mockito.when(bun.getName()).thenReturn("Тестовая булочка");
+        Mockito.when(ingredient.getName()).thenReturn("Тестовый ингридиент");
+        Mockito.when(ingredient.getType()).thenReturn(IngredientType.SAUCE);
+        Mockito.when(burger.getPrice()).thenReturn(5f);
+        System.out.println(burger.getReceipt());
+        System.out.println(burger.getPrice());
     }
 }
