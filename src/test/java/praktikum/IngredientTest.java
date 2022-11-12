1cbacec972f3644715158ed7ca8f21aeadc44a6b
Sergei Trofimov <divisor.null@yandex.com>
2022-11-12 18:02:02 +0300

    Правки перед сдачей

--- src/test/java/praktikum/IngredientTest.java
+++ src/test/java/praktikum/IngredientTest.java
@@ -31,19 +31,19 @@ public class IngredientTest {
 
     @Test // У ингридиента правилен верный тип
     public void getIngredientTypeTest() {
-       Ingredient ingredient = new Ingredient(type, name, price);
+        Ingredient ingredient = new Ingredient(type, name, price);
         assertEquals("Имя объекта и введеное имя не совпали", type, ingredient.getType());
     }
 
     @Test // Ингридиенту присвоено правильное имя
     public void getIngredientNameTest() {
-       Ingredient ingredient = new Ingredient(type, name, price);
+        Ingredient ingredient = new Ingredient(type, name, price);
         assertEquals("Имя объекта и введеное имя не совпали", name, ingredient.getName());
     }
 
     @Test // Ингридиенту проставлена верная цена
     public void getIngredientTest() {
-       Ingredient ingredient = new Ingredient(type, name, price);
+        Ingredient ingredient = new Ingredient(type, name, price);
         assertEquals("Цена объекта и введеная цена не совпали", price, ingredient.getPrice(), 0);
     }
 }
